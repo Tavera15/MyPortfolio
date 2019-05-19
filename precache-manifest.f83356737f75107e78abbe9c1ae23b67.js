832834b654952f40e593b694d5bf753f6f0f0f1d
Joaquin Ramirez <joaqu@LAPTOP-FH5918UE>
2019-05-18 20:49:16 -0500

    Updates

---
+++ precache-manifest.f83356737f75107e78abbe9c1ae23b67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87333b9624426ce21aa24377b7c3d3e",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "4e0b555ab0e01283ee2a",
+    "url": "/MyPortfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8c594e3ebbe081164845",
+    "url": "/MyPortfolio/static/css/main.14011f64.chunk.css"
+  },
+  {
+    "revision": "4e0b555ab0e01283ee2a",
+    "url": "/MyPortfolio/static/js/2.f139367a.chunk.js"
+  },
+  {
+    "revision": "8c594e3ebbe081164845",
+    "url": "/MyPortfolio/static/js/main.715f3f80.chunk.js"
+  },
+  {
+    "revision": "41ef2c5e8e68d7a2a057",
+    "url": "/MyPortfolio/static/js/runtime~main.ccce04bf.js"
+  },
+  {
+    "revision": "827ccf0c29e7b23fd1b5e4f204405213",
+    "url": "/MyPortfolio/static/media/logo.827ccf0c.png"
+  },
+  {
+    "revision": "f34d3f6f6f7cd18883ef25f4ed027c2e",
+    "url": "/MyPortfolio/static/media/mainBG.f34d3f6f.jpg"
+  },
+  {
+    "revision": "38f5893e08dadaaa2a5fe735515b4280",
+    "url": "/MyPortfolio/static/media/re7.38f5893e.otf"
+  }
+]);
