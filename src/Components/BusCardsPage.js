6793f3ac5c2c913d778a0150aee830f5bf93b180
Joaquin Ramirez <joaqu@LAPTOP-FH5918UE>
2019-06-09 23:36:04 -0500

    Add HR Business Card

--- src/Components/BusCardsPage.js
+++ src/Components/BusCardsPage.js
@@ -7,13 +7,20 @@ const BusCardsPage = () => {
 	      	<h1>Business Cards</h1>
 	      	<div className="projects-section">
 
-				<div className="card col-lg-4 col-md-6">
-					<img src={require('../contents/JRCard.png')} className="card-img-top" alt="JRlogo"/>				    
-					  <div className="card-body">
-					      <h3 className="card-title">Joaquin Ramirez Pool Service</h3>
-			    	  </div>
+	      		<div className="row">
+					<div className="card col-lg-4 col-md-6">
+						<img src={require('../contents/JRCard.png')} className="card-img-top" alt="JRlogo"/>				    
+						  <div className="card-body">
+						      <h3 className="card-title">Joaquin Ramirez Pool Service</h3>
+				    	  </div>
+					 </div>
+					 <div className="card col-lg-4 col-md-6">
+						<img src={require('../contents/Hilario Card.png')} className="card-img-top" alt="HRlogo"/>				    
+						  <div className="card-body">
+						      <h3 className="card-title">Hilario Ramirez Pool Service</h3>
+				    	  </div>
+					 </div>
 				 </div>
-
 	      	</div>
 	      </div>
 
