e1ca18592ffa97f62cff421fc52bf2e8f3f9dfd8
Joaquin Ramirez <joaqu@LAPTOP-FH5918UE>
2020-03-31 22:08:15 -0500

    Redirect to new website

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import TopBar from './Components/TopBar.js';
-import Home from './Pages/Home.js';
-import UnrealPage from './Pages/UEPage.js';
-import WebDevPage from './Pages/WebDevPage.js';
-import Unity from './Pages/UnityPage.js';
-import CSharpPage from './Pages/C#Page.js';
-import CppPage from './Pages/CppPage.js';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import TopBar from './Components/TopBar.js';
+// import Home from './Pages/Home.js';
+// import UnrealPage from './Pages/UEPage.js';
+// import WebDevPage from './Pages/WebDevPage.js';
+// import Unity from './Pages/UnityPage.js';
+// import CSharpPage from './Pages/C#Page.js';
+// import CppPage from './Pages/CppPage.js';
 import './App.css';
 
 function App()
 {
     return (
-      <Router>
-        <div>
-          <TopBar />
-
-          <Route exact path="/MyPortfolio" component={Home} />
-          <Route exact path="/MyPortfolio/Unreal" component={UnrealPage} />
-          <Route exact path="/MyPortfolio/WebDevelopment" component={WebDevPage} />
-          <Route exact path="/MyPortfolio/CSharp" component={CSharpPage} />
-          <Route exact path="/MyPortfolio/Unity" component={Unity} />
-          <Route exact path="/MyPortfolio/CPlusPlus" component={CppPage} />
-
-        </div>
-      </Router>
+      <h1 style={{textAlign: 'center', padding: "40vh 0", fontFamily: 'ResidentEvil', fontSize: '80px'}}>Website moved <a style={{color: 'red', textDecoration: 'underline'}}  target="_blank" rel="noopener noreferrer" href="https://joaquinramireztavera.com/">Here</a></h1>
     );
   
 }
